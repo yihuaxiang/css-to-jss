393d0b7162cf22298e728d30548a23925f8b9e47
yihuaxiang <fudongdonguestc@gmail.com>
2022-11-20 14:53:36 +0800

    css-to-jss api

---
+++ index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const {toJSS} = require("./transform");
+const app = new Koa();
+
+app.use(async ctx => {
+  const styleQuery = ctx.query.style;
+  if(styleQuery) {
+    ctx.body = toJSS(styleQuery);
+  } else {
+    ctx.body = '请输入 style 参数'
+  }
+});
+
+app.listen(5000);
---
+++ transform.js
@@ -0,0 +1,17 @@
+const postcss = require('postcss');
+const postcssJs = require("postcss-js");
+
+const toJSSObject = (cssText) => {
+  const root = postcss.parse(cssText);
+  return postcssJs.objectify(root);
+};
+
+module.exports = {
+  toJSS: (cssText) => {
+    try {
+      return JSON.stringify(toJSSObject(cssText), null, 2);
+    } catch (e) {
+      return "Error translating CSS to JSS";
+    }
+  }
+}
